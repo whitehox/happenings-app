8ab5ed4848f8c96ff684636d158a6cd3d16426e8
josemak25 <amakirij@gmail.com>
2020-01-16 18:04:56 +0100

    add button component styles

---
+++ components/button/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.TouchableOpacity`
+  width: 100;
+  min-height: 50;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+`;
