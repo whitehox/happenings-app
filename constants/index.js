2774198e9bf3c63ce5fb57ba59692eabdddddc26
josemak25 <amakirij@gmail.com>
2020-01-16 18:05:47 +0100

    add fonts, color and shared component header styles

---
+++ constants/index.js
@@ -0,0 +1,32 @@
+// All app colors
+export const colors = {
+  BACKGROUND_COLOR: "#FFFFFF",
+  MAP_BACKGROUND_COLOR: "#272B4A",
+  CARD_SHADOW_COLOR: "#DEE7EC",
+  FONT_COLOR: "#CCCED5"
+};
+
+// All app font sizes
+export const fonts = {
+  FONT_SIZE_SMALL: 12,
+  FONT_SIZE_MEDIUM: 15,
+  FONT_SIZE_LARGE: 16,
+  FONT_SIZE_EXTRA_LARGE: 24,
+  FONT_FAMILY_BOLD: "panton-bold",
+  FONT_FAMILY_REGULAR_BOLD: "panton-bold-regular",
+  FONT_FAMILY_REGULAR: "panton-regular",
+  FONT_FAMILY_LIGHT: "panton-light",
+  FONT_WEIGHT_LIGHT: 200,
+  FONT_WEIGHT_MEDIUM: 600,
+  FONT_WEIGHT_HEAVY: 800
+};
+
+// App header custom style
+export const customHeaderStyle = {
+  borderBottomWidth: 0,
+  elevation: 0,
+  shadowOpacity: 0
+};
+
+// App theme
+export default theme = { colors, fonts, customHeaderStyle };
