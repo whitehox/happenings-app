826f2f0c7cac02c9ac75fee1b893721698fe7c91
Tolu Adesina <tadesina90@gmail.com>
2020-01-22 22:00:46 +0100

    style the list screen

---
+++ screens/list/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { colors, fonts } from "../../constants";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.BACKGROUND_COLOR};
+`;
+
+export const TextMid = styled.Text`
+  font-size: ${fonts.FONT_SIZE_LARGE};
+  font-family: ${fonts.FONT_FAMILY_REGULAR_BOLD};
+`;
