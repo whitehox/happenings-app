ed81d938a88dc964731c4655d956d975d8444d4a
whitehox <oketegah@gmail.com>
2020-01-23 06:18:44 +0100

    converted stack navigator to drawer navigator

--- router.js
+++ router.js
@@ -1,21 +1,24 @@
-import { createStackNavigator } from "react-navigation-stack";
-import { createAppContainer } from "react-navigation";
-import { customHeaderStyle } from "./constants";
+import { createAppContainer } from 'react-navigation';
+import { customHeaderStyle } from './constants';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 
-import Screens from "./screens";
+import Screens from './screens';
+import SideDrawer from './components/SideDrawer';
 
-const AppNavigator = createStackNavigator(
+const AppNavigator = createDrawerNavigator(
   {
     // Home Route
-    HomeScreen: { screen: Screens.HomeScreen }
+    HomeScreen: { screen: Screens.HomeScreen },
   },
 
   {
-    initialRouteName: "HomeScreen",
+    initialRouteName: 'HomeScreen',
     defaultNavigationOptions: {
-      headerStyle: customHeaderStyle
-    }
-  }
+      headerStyle: customHeaderStyle,
+    },
+    contentComponent: SideDrawer,
+    drawerWidth: 300,
+  },
 );
 
 export default AppRoutes = createAppContainer(AppNavigator);
