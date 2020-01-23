ea09e4147edec01405eae79d7c0240b84c1a193a
josemak25 <amakirij@gmail.com>
2020-01-23 19:39:38 +0100

    add user profile component

---
+++ components/profile/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Card from "../card";
+import MaleAvatar from "../../assets/images/maleAvatar";
+import FemaleAvatar from "../../assets/images/femaleAvatar";
+
+import { cardStyles } from "./styles";
+
+export default Profile = ({ getParam }) => {
+  const userType = getParam("userType") || "male";
+
+  return (
+    <Card styles={cardStyles}>
+      {userType === "male" ? <MaleAvatar /> : <FemaleAvatar />}
+    </Card>
+  );
+};
