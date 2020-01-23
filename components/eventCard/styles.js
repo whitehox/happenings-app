66e4518d5d6643afee4e843c002386113a7c4e64
Tolu Adesina <tadesina90@gmail.com>
2020-01-23 12:45:59 +0100

    format the event card styling

--- components/eventCard/styles.js
+++ components/eventCard/styles.js
@@ -1,13 +1,12 @@
-import {StyleSheet} from 'react-native'
-import styled from 'styled-components'
-import {fonts, colors} from '../../constants';
+import styled from "styled-components";
+import { fonts, colors } from "../../constants";
 
 export const Container = styled.View`
   flex-direction: row;
   width: 100%;
   justify-content: space-between;
   margin: 10px 0;
-  border: 1px solid ${colors.CARD_SHADOW_COLOR}
+  border: 1px solid ${colors.CARD_SHADOW_COLOR};
 `;
 
 export const EventDateBox = styled.View`
@@ -33,9 +32,7 @@ export const Month = styled.Text`
   color: ${colors.WHITE};
 `;
 
-export const FavEvent = styled.View`
-
-`;
+export const FavEvent = styled.View``;
 
 export const EventCardInfo = styled.View`
   flex-grow: 0;
@@ -50,4 +47,4 @@ export const EventTitle = styled.Text`
 
 export const eventCardStyle = {
   borderRadius: 5
-}
+};
