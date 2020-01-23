270a0c90d8c55e93795886714468f6f2274bb5a0
Tolu Adesina <tadesina90@gmail.com>
2020-01-23 10:23:05 +0100

    create event card component to display the events

---
+++ components/eventCard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, EventCardInfo, EventDate, EventDateBox } from "./styles";
+
+const EventCard = props => {
+  const { eventDay, eventTitle } = props;
+  return (
+    <Container>
+      <EventDateBox>
+        <EventDate>{eventDay}</EventDate>
+      </EventDateBox>
+      <EventCardInfo>
+        <EventTitle>{eventTitle}</EventTitle>
+      </EventCardInfo>
+    </Container>
+  );
+};
+
+export default EventCard;
