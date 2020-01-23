72eda6c265a7f733946d78e5ae7fe7aea63ee576
Tolu Adesina <tadesina90@gmail.com>
2020-01-23 16:36:37 +0100

    pack all styles that are not styled component into a styles object

--- components/eventCard/styles.js
+++ components/eventCard/styles.js
@@ -67,26 +67,30 @@ export const EventTitle = styled.Text`
   color: ${colors.WHITE};
 `;
 
-export const eventCardStyle = {
-  borderTopLeftRadius: 10,
-  borderTopRightRadius: 0,
-  borderBottomRightRadius: 0,
-  borderBottomLeftRadius: 10,
-  height: 250,
-  justifyContent: 'flex-start',
-};
-
-export const eventImageStyle = {
-  width: '100%',
-  height: 150,
-  justifyContent: 'flex-end'
-}
-
 export const EventMiniInfoView = styled.View`
   width: 100%;
   padding: 10px;
-`
+`;
 
-export const eventInfoRow = {
-  justifyContent: 'space-between'
+export const styles = {
+  eventCardStyle: {
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    borderBottomLeftRadius: 10,
+    height: 250,
+    justifyContent: 'flex-start',
+  },
+  eventImageStyle: {
+    width: '100%',
+    height: 150,
+    justifyContent: 'flex-end'
+  },
+  eventInfoRow: {
+    justifyContent: 'space-between'
+  },
+  eventLocation: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 }
